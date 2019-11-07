70ea7bc70d7e78242611f0906cc33794f0ccac1d
travistower <flipp151@hotmail.com>
2019-11-07 11:23:41 -0500

    my stuff

--- ItemFinder/Cool.cs
+++ ItemFinder/Cool.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 
 namespace ItemFinder
 {
@@ -11,5 +12,6 @@ namespace ItemFinder
         private int gy;
 
         private int hfjsdhj;
+        private int anotherthing;
     }
 }
