222070c40b0d3623660f972320c1ef3b358b1a07
travistower <flipp151@hotmail.com>
2019-11-13 13:39:28 -0500

    searching kinda working

--- ItemFinder/UserForm.aspx
+++ ItemFinder/UserForm.aspx
@@ -9,7 +9,38 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <label>Search</label>
+            <asp:TextBox ID="TxtSearch" runat="server" OnTextChanged="TxtSearch_OnTextChanged"></asp:TextBox>
         </div>
+        
+        <div>
+            <asp:Button ID="BtnSearch" runat="server" Text="Button" />
+        </div>
+
+        <div>
+            <asp:Image ID="ImgMap" runat="server" />
+        </div>
+        
+        <div>
+            <asp:Label ID="LblName" runat="server" Text="Label"></asp:Label>
+        </div>
+        
+        <div>
+            <asp:Label ID="LblDept" runat="server" Text="Label"></asp:Label>
+        </div>
+        
+        <div>
+            <asp:Label ID="LblDesc" runat="server" Text="Label"></asp:Label>
+        </div>
+        
+        <div>
+            <asp:Label ID="LblPrice" runat="server" Text="Label"></asp:Label>
+        </div>
+
+        <div>
+            <asp:Button ID="BtnAddItem" runat="server" Text="Button" />
+        </div>
+
     </form>
 </body>
 </html>
--- ItemFinder/UserForm.aspx.cs
+++ ItemFinder/UserForm.aspx.cs
@@ -1,17 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ItemFinder.ItemFinderDataSetTableAdapters;
 
 namespace ItemFinder
 {
     public partial class UserForm : System.Web.UI.Page
     {
+        ItemFinderDataSet.StoreDataTable _storeDataTable = new ItemFinderDataSet.StoreDataTable();
+        ItemFinderDataSet.DepartmentDataTable _departmentDataTable = new ItemFinderDataSet.DepartmentDataTable();
+        ItemFinderDataSet.ItemDataTable _itemDataTable = new ItemFinderDataSet.ItemDataTable();
+
+
+        ItemTableAdapter _itemTableAdapter = new ItemTableAdapter();
+        ItemFinderDataSet _dataSet = new ItemFinderDataSet();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            _itemTableAdapter.Fill(_dataSet.Item);
+            _itemDataTable = _itemTableAdapter.GetData();
 
+            if (!IsPostBack)
+            {
+                _itemDataTable = _itemTableAdapter.GetData();
+                //var items = _itemDataTable.Rows[0].Field<string>(2); //how to get a row
+            }
+        }
+
+        protected void TxtSearch_OnTextChanged(object sender, EventArgs e)
+        {
+            var searchString = TxtSearch.Text;
+            var filtered = _itemDataTable.AsEnumerable()
+                .Where(r => r.Field<string>("ItemName").Contains(searchString));
+
+            var filteredList = filtered.ToList();
+
+            //LblName.Text = _itemDataTable.ItemNameColumn.ToString();
+            if (filteredList.Count > 0)
+                LblName.Text = filteredList[0].ItemName;
+            else
+                LblName.Text = "";
         }
     }
 }
